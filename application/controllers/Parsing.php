360d809b509dc14d17091b150df2550c0f017adb
entz180630 <entz180630@gmail.com>
2023-02-09 17:15:38 +0900

    commit

--- application/controllers/Parsing.php
+++ application/controllers/Parsing.php
@@ -7,7 +7,7 @@ class Parsing extends CI_Controller {
         public function crolling(){
                 $url = "http://depart2.studio.takeone.co.kr/pucca/buildqa/v1/packet"
                 $data = file_get_contents($url);
-                var_dump($data);
+                echo $data;
         }
 
 }
