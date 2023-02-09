fd46df6215e8007261bd8e211e61d9aa848016bd
entz180630 <entz180630@gmail.com>
2023-02-09 17:16:53 +0900

    commit

--- application/controllers/Parsing.php
+++ application/controllers/Parsing.php
@@ -5,6 +5,8 @@ include APPPATH . 'third_party/simple_html_dom.php';
 class Parsing extends CI_Controller {
        
         public function crolling(){
+                $data = '';
+                
                 $url = "http://depart2.studio.takeone.co.kr/pucca/buildqa/v1/packet"
                 $data = file_get_contents($url);
                 echo $data;
