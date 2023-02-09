db90942448bb7ccd2a2fd95dded2f247f6e39b80
entz180630 <entz180630@gmail.com>
2023-02-09 17:14:46 +0900

    var use

--- application/controllers/Parsing.php
+++ application/controllers/Parsing.php
@@ -5,7 +5,8 @@ include APPPATH . 'third_party/simple_html_dom.php';
 class Parsing extends CI_Controller {
        
         public function crolling(){
-                $data = file_get_contents("http://depart2.studio.takeone.co.kr/pucca/buildqa/v1/packet");
+                $url = "http://depart2.studio.takeone.co.kr/pucca/buildqa/v1/packet"
+                $data = file_get_contents($url);
                 var_dump($data);
         }
 
