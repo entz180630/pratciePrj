afc71dec279bc1a8875e7298073b844548819b1c
entz180630 <entz180630@gmail.com>
2023-02-09 12:40:06 +0900

    first commit

--- application/controllers/Ppa.php
+++ application/controllers/Ppa.php
@@ -5,6 +5,6 @@ class Ppa extends CI_Controller {
        
         public function ppa_page1(){
 
-                echo "page is ppa";
+                echo "page is ppa at vscode use git";
         }
 }
