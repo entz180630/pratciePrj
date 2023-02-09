3798c9181b1fe0411fd10d251317b1eab5d76aac
entz180630 <entz180630@gmail.com>
2023-02-09 17:00:02 +0900

    url change

--- application/controllers/Parsing.php
+++ application/controllers/Parsing.php
@@ -5,7 +5,7 @@ include APPPATH . 'third_party/simple_html_dom.php';
 class Parsing extends CI_Controller {
        
         public function crolling(){
-                $data = file_get_html("http://172.27.10.6/pucca/buildqa/v1/packet");
+                $data = file_get_html("http://depart2.studio.takeone.co.kr/pucca/buildqa/v1/packet");
                 var_dump($data);
         }
 
