c8c4c3c884fd286ad0b7dfdffe891c7298566a07
entz180630 <entz180630@gmail.com>
2023-02-09 17:44:49 +0900

    url change

--- application/controllers/Parsing.php
+++ application/controllers/Parsing.php
@@ -7,9 +7,14 @@ class Parsing extends CI_Controller {
         public function crolling(){
                 $data = '';
 
-                $url = "http://172.27.10.6:80/pucca/buildqa/v1/packet";
+                // $url = "http://172.27.10.6:80/pucca/buildqa/v1/packet";
+                $url = "https://www.kebhana.com/cont/mall/mall15/mall1501/index.jsp?_menuNo=23100&#35;//HanaBank";
                 $data = file_get_contents($url);
                 echo $data;
         }
 
 }
+
+
+
+//http://depart2.studio.takeone.co.kr/pucca/buildqa/v1/test/cheat/%7B%22uid%22:%22580%22,%20%22token%22:%22%22,%20%22cheat%22:%22timer%20NOW%22%7D
