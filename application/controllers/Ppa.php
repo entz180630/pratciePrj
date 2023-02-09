443bd0306482fe45b57eb8cb70c559bf5715ce45
entz180630 <entz180630@gmail.com>
2023-02-09 01:42:47 +0000

    first commit

---
+++ application/controllers/Ppa.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ppa extends CI_Controller {
+       
+        public function ppa_page1(){
+
+                echo "page is ppa";
+        }
+}
